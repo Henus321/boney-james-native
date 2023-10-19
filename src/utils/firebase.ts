240b583fb4ffa80a9f012bd9eb2a964e32a29bdc
Alexander Erkhov <tyrantbud@yandex.ru>
2023-10-19 21:20:38 +0300

    Added firebase auth settings

--- src/utils/firebase.ts
+++ src/utils/firebase.ts
@@ -1,6 +1,11 @@
 import {initializeApp} from 'firebase/app';
 import {getFirestore} from 'firebase/firestore';
-import {getAuth} from 'firebase/auth';
+import {
+  initializeAuth,
+  getAuth,
+  getReactNativePersistence,
+} from 'firebase/auth';
+import ReactNativeAsyncStorage from '@react-native-async-storage/async-storage';
 
 const firebaseConfig = {
   apiKey: String(process.env.REACT_APP_FIREBASE_API_KEY),
@@ -11,7 +16,11 @@ const firebaseConfig = {
   appId: '1:950740856907:web:27644bb841e6d02a035dce',
 };
 
-initializeApp(firebaseConfig);
+const app = initializeApp(firebaseConfig);
+
+initializeAuth(app, {
+  persistence: getReactNativePersistence(ReactNativeAsyncStorage),
+});
 
 export const db = getFirestore();
 
